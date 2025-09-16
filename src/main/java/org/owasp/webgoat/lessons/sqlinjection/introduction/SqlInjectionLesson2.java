db44b85813da1b85d1f1fd696f47f95bc2f9c1a6
sast-qa <123078419+sast-qa@users.noreply.github.com>
2025-09-16 15:30:31 +0300

    Add/Update src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java file to repo.

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
@@ -1,78 +1,247 @@
+import datetime
+import re
+import subprocess
+from hashlib import md5
 
-/*
- * This file is part of WebGoat, an Open Web Application Security Project utility. For details, please see http://www.owasp.org/
- *
- * Copyright (c) 2002 - 2019 Bruce Mayhew
- *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program; if
- * not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- *
- * Getting Source ==============
- *
- * Source for this application is maintained at https://github.com/WebGoat/WebGoat, a repository for free software projects.
- */
+import jwt
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
-package org.owasp.webgoat.lessons.sqlinjection.introduction;
+from .models import CSRF_user_tbl
+from .views import authentication_decorator
 
-import org.owasp.webgoat.container.LessonDataSource;
-import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
-import org.owasp.webgoat.container.assignments.AssignmentHints;
-import org.owasp.webgoat.container.assignments.AttackResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+# import os
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+## Mitre top1 | CWE:787
 
-import static java.sql.ResultSet.CONCUR_READ_ONLY;
-import static java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE;
+# target zone
+FLAG = "NOT_SUPPOSED_TO_BE_ACCESSED"
+
+# target zone end
 
 
-@RestController
-@AssignmentHints(value = {"SqlStringInjectionHint2-1", "SqlStringInjectionHint2-2", "SqlStringInjectionHint2-3", "SqlStringInjectionHint2-4"})
-public class SqlInjectionLesson2 extends AssignmentEndpoint {
+@authentication_decorator
+def mitre_top1(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top1.html')
 
-    private final LessonDataSource dataSource;
+@authentication_decorator
+def mitre_top2(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top2.html')
 
-    public SqlInjectionLesson2(LessonDataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+@authentication_decorator
+def mitre_top3(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top3.html')
+        
+@authentication_decorator
+def mitre_top4(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top4.html')
+        
+@authentication_decorator
+def mitre_top5(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top5.html')
+        
+@authentication_decorator
+def mitre_top6(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top6.html')
+        
+@authentication_decorator
+def mitre_top7(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top7.html')
+        
+@authentication_decorator
+def mitre_top8(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top8.html')
+        
+@authentication_decorator
+def mitre_top9(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top9.html')
+        
+@authentication_decorator
+def mitre_top10(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top10.html')
+        
+@authentication_decorator
+def mitre_top11(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top11.html')
+        
+@authentication_decorator
+def mitre_top12(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top12.html')
+        
+@authentication_decorator
+def mitre_top13(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top13.html')
+        
+@authentication_decorator
+def mitre_top14(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top14.html')
 
-    @PostMapping("/SqlInjection/attack2")
-    @ResponseBody
-    public AttackResult completed(@RequestParam String query) {
-        return injectableQuery(query);
-    }
+@authentication_decorator
+def mitre_top15(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top15.html')
 
-    protected AttackResult injectableQuery(String query) {
-        try (var connection = dataSource.getConnection()) {
-            Statement statement = connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
-            ResultSet results = statement.executeQuery(query);
-            StringBuilder output = new StringBuilder();
+@authentication_decorator
+def mitre_top16(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top16.html')
 
-            results.first();
+@authentication_decorator
+def mitre_top17(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top17.html')
 
-            if (results.getString("department").equals("Marketing")) {
-                output.append("<span class='feedback-positive'>" + query + "</span>");
-                output.append(SqlInjectionLesson8.generateTable(results));
-                return success(this).feedback("sql-injection.2.success").output(output.toString()).build();
-            } else {
-                return failed(this).feedback("sql-injection.2.failed").output(output.toString()).build();
+@authentication_decorator
+def mitre_top18(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top18.html')
+
+@authentication_decorator
+def mitre_top19(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top19.html')
+
+
+@authentication_decorator
+def mitre_top20(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top20.html')
+
+
+@authentication_decorator
+def mitre_top21(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top21.html')
+
+
+@authentication_decorator
+def mitre_top22(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top22.html')
+
+
+@authentication_decorator
+def mitre_top23(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top23.html')
+
+
+@authentication_decorator
+def mitre_top24(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top24.html')
+
+@authentication_decorator
+def mitre_top25(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top25.html')
+
+@authentication_decorator
+def csrf_lab_login(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/csrf_lab_login.html')
+    elif request.method == 'POST':
+        password = request.POST.get('password')
+        username = request.POST.get('username')
+        password = md5(password.encode()).hexdigest()
+        User = CSRF_user_tbl.objects.filter(username=username, password=password)
+        if User:
+            payload ={
+                'username': username,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=300),
+                'iat': datetime.datetime.utcnow()
             }
-        } catch (SQLException sqle) {
-            return failed(this).feedback("sql-injection.2.failed").output(sqle.getMessage()).build();
-        }
-    }
-}
+            cookie = jwt.encode(payload, 'csrf_vulneribility', algorithm='HS256')
+            response = redirect("/mitre/9/lab/transaction")
+            response.set_cookie('auth_cookiee', cookie)
+            return response
+        else :
+            return redirect('/mitre/9/lab/login')
+
+@authentication_decorator
+@csrf_exempt
+def csrf_transfer_monei(request):
+    if request.method == 'GET':
+        try:
+            cookie = request.COOKIES['auth_cookiee']
+            payload = jwt.decode(cookie, 'csrf_vulneribility', algorithms=['HS256'])
+            username = payload['username']
+            User = CSRF_user_tbl.objects.filter(username=username)
+            if not User:
+                redirect('/mitre/9/lab/login')
+            return render(request, 'mitre/csrf_dashboard.html', {'balance': User[0].balance})
+        except:
+            return redirect('/mitre/9/lab/login')
+
+def csrf_transfer_monei_api(request,recipent,amount):
+    if request.method == "GET":
+        cookie = request.COOKIES['auth_cookiee']
+        payload = jwt.decode(cookie, 'csrf_vulneribility', algorithms=['HS256'])
+        username = payload['username']
+        User = CSRF_user_tbl.objects.filter(username=username)
+        if not User:
+            return redirect('/mitre/9/lab/login')
+        if int(amount) > 0:
+            if int(amount) <= User[0].balance:
+                recipent = CSRF_user_tbl.objects.filter(username=recipent)
+                if recipent:
+                    recipent = recipent[0]
+                    recipent.balance = recipent.balance + int(amount)
+                    recipent.save()
+                    User[0].balance = User[0].balance - int(amount)
+                    User[0].save()
+        return redirect('/mitre/9/lab/transaction') 
+    else:
+        return redirect ('/mitre/9/lab/transaction')
+
+
+# @authentication_decorator
+@csrf_exempt
+def mitre_lab_25_api(request):
+    if request.method == "POST":
+        expression = request.POST.get('expression')
+        result = eval(expression)
+        return JsonResponse({'result': result})
+    else:
+        return redirect('/mitre/25/lab/')
+
+
+@authentication_decorator
+def mitre_lab_25(request):
+    return render(request, 'mitre/mitre_lab_25.html')
+
+@authentication_decorator
+def mitre_lab_17(request):
+    return render(request, 'mitre/mitre_lab_17.html')
+
+def command_out(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process.communicate()
+    
+
+@csrf_exempt
+def mitre_lab_17_api(request):
+    if request.method == "POST":
+        ip = request.POST.get('ip')
+        command = "nmap " + ip 
+        res, err = command_out(command)
+        res = res.decode()
+        err = err.decode()
+        pattern = "STATE SERVICE.*\\n\\n"
+        ports = re.findall(pattern, res,re.DOTALL)[0][14:-2].split('\n')
+        return JsonResponse({'raw_res': str(res), 'raw_err': str(err), 'ports': ports})
